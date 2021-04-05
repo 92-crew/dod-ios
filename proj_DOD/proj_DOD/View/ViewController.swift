decfabf1e4d4497bd5dcc9bcf7442835a8e974c1
Kangwook Lee <zu7342@gmail.com>
2021-04-06 00:33:15 +0900

    #3 test commit

--- proj_DOD/proj_DOD/View/ViewController.swift
+++ proj_DOD/proj_DOD/View/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var tableView: UITableView = UITableView()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    override func loadView() {
+        super.loadView()
+        view.addSubview(tableView)
+    }
 
 }
 
