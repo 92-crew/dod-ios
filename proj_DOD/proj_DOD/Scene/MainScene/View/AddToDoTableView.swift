06255cea3ec3508b2c787aeff45c30acca8c2964
Kangwook Lee <zu7342@gmail.com>
2021-05-02 23:40:59 +0900

    add view controller 작성, model 수정

---
+++ proj_DOD/proj_DOD/Scene/MainScene/View/AddToDoTableView.swift
@@ -0,0 +1,8 @@
+//
+//  AddToDoTableView.swift
+//  proj_DOD
+//
+//  Created by 이강욱 on 2021/05/02.
+//
+
+import Foundation
---
+++ proj_DOD/proj_DOD/Scene/MainScene/ViewModel/ViewModel/AddViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AddViewModel.swift
+//  proj_DOD
+//
+//  Created by 이강욱 on 2021/05/02.
+//
+
+import Foundation
